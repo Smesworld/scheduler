b211e6c8cb137b958d5e49b0c6445255e41889d6
Alan Dodge <alphaechododge@gmail.com>
2019-10-28 12:21:38 -0700

    Added stories and formatting to DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,17 +4,31 @@ import classNames from 'classnames';
 
 import "components/DayListItem.scss";
 
+const formatSpots = (spots) => {
+  let spotString;
+  if (spots > 1) {
+    spotString = `${spots} spots remaining`;
+  } else if (spots === 1) {
+    spotString = `${spots} spot remaining`;
+  } else {
+    spotString = `no spots remaining`
+  }
+
+  return spotString;
+}
+
+
+
 export default function DayListItem(props) {
   const dayClass = classNames( "day-list__item", {
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : props.spots === 0
- });
-
+  });
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining.</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
